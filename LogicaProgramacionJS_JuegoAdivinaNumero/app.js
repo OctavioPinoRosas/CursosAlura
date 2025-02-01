a5af0305b74eaa97b1a02971a68d1b41c8989f31
Octavio <octaviopinoross@gmail.com>
2025-02-01 00:36:44 -0600

    app.js secretNumberGame DiveIntoProgrammingLogicJS

--- LogicaProgramacionJS_JuegoAdivinaNumero/app.js
+++ LogicaProgramacionJS_JuegoAdivinaNumero/app.js
@@ -1,8 +1,29 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+//Variables
+numero_maximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numero_maximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+let maximosIntentos =6;
 
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numero_maximoPosible} por favor:`));
+    console.log(numeroSecreto);
+    if (numeroUsuario == numeroSecreto) {
+        //Es verdadera la condicion, se acierta el numero
+        alert(`Acertaste, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    }
+    else { //No se acierta
+        if (numeroUsuario > numeroSecreto) {//El numero secreto es menor
+            alert('El número secreto es menor');
+        }
+        else {//El numero secreto es mayor
+            alert('El número secreto es mayor');
+        }
+        intentos++;
+        //Si se superan el maximo de intentos
+        if (intentos > maximosIntentos){
+            alert(`llegaste al numero maximo de ${maximosIntentos} intentos`)
+            break;
+        }
+    }
 }
