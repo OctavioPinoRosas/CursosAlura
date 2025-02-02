078c1349ac201d7b2b2e9d456d9b5f59153cfa68
Octavio <octaviopinoross@gmail.com>
2025-02-01 23:04:13 -0600

    Add funtions 'asignarTextoElemento' and 'intentoUsuario'

---
+++ LogicaProgramacion2/app.js
@@ -0,0 +1,10 @@
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+function intentoDeUsuario(){
+    alert('Clic desde el boton')
+}
+
+asignarTextoElemento('h1','Juego del número secreto');
+asignarTextoElemento('p','Indica un número del 1 al 100');
