1104133e8192ceb6da09ad46010cf22485c643fd
Octavio <octaviopinoross@gmail.com>
2025-02-03 21:57:11 -0600

    Add action to buttons

--- LogicaProgramacion2/app.js
+++ LogicaProgramacion2/app.js
@@ -1,10 +1,56 @@
+let numeroSecreto = 0;
+let intentos = 0;
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
-}
-function intentoDeUsuario(){
-    alert('Clic desde el boton')
+    return;
 }
 
-asignarTextoElemento('h1','Juego del número secreto');
-asignarTextoElemento('p','Indica un número del 1 al 100');
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroSecreto);
+    console.log(intentos);
+    if (numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p','El número secreto es menor');
+        } else{
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiaCaja();
+    }
+    return;
+}
+
+function limpiaCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p','Indica un número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    //Limpiar la caja
+    limpiaCaja();
+    //Indicar mensaje de intervalo de números
+    //Generar el numero aleatorio
+    //Iniciar el numero de intentos
+    condicionesIniciales();
+    //Deshabilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+condicionesIniciales();
