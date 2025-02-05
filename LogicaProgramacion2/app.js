c486a07bedd9b43cc1e909dd94cfe9826ab95dfb
Octavio <octaviopinoross@gmail.com>
2025-02-04 22:22:07 -0600

    feat: prevent number repetition in generarNumeroSecreto by using an array

--- LogicaProgramacion2/app.js
+++ LogicaProgramacion2/app.js
@@ -1,5 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 3;
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -9,8 +11,6 @@ function asignarTextoElemento(elemento, texto){
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(numeroSecreto);
-    console.log(intentos);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
@@ -32,12 +32,26 @@ function limpiaCaja(){
 }
 
 function generarNumeroSecreto(){
-    return Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los numeros
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else{
+        //Si el numero generado esta incluido en la lista
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        } else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
 
 function condicionesIniciales(){
     asignarTextoElemento('h1','Juego del número secreto');
-    asignarTextoElemento('p','Indica un número del 1 al 10');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
